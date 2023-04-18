6c26841de0ca65c58960e6ae2d2a78743a2092e0
David Ramlakhan <dram1964@gmail.com>
2023-04-18 10:41:26 +0100

    Adding Git Install notes to README

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Powershell-Scripts
+
 Powershell Scripts for VM Customisations
 
 Use the scripts in this repository with 
 Powershell customisers in your Azure 
 Compute Gallery customisations
+
+
+## Silent Git installs
+
+Best option for configuring settings for an unattended git install is:
+
+1. Download the git executable
+2. Run the executable with the /SAVEINF=<filename> option
+3. The saved file <filename> can now be used with /LOADINF=<filename> in your unattended install
+
+Full options for an unattended install are listed [here](https://jrsoftware.org/ishelp/index.php?topic=setupcmdline):
+
+```
+"/NORESTART /VERYSILENT /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOG=$BUILD_DIRECTORY\git-for-windows.log /LOADINF=$BUILD_DIRECTORY\add_bash.inf /SUPPRESSMSGBOXES /ALLUSERS"
+```
+Further info at:
+
+- [StackOverflow](https://stackoverflow.com/questions/73285729/how-can-i-install-git-with-bash-on-powershell-on-powershell)
+- [Git for Windows](https://github.com/git-for-windows/git/wiki/Silent-or-Unattended-Installation)
+- [New Context](https://github.com/newcontext-oss/add-git/blob/master/Add-Git.psm1)
--- add_bash.ps1
+++ add_bash.ps1
@@ -8,11 +8,40 @@ function Write-Log {
 
 $BUILD_DIRECTORY="C:\BuildArtifacts"
 $INSTALL_DIRECTORY="C:\Software"
+$SETTINGS_FILE="$BUILD_DIRECTORY\bash_settings.inf"
 
 # Git Bash
 $GitBash_INSTALLER_FILE="Git-2.40.0-64-bit.exe"
 $GitBash_DOWNLOAD_URL="https://github.com/git-for-windows/git/releases/download/v2.40.0.windows.1/$GitBash_INSTALLER_FILE"
-$GitBash_INSTALL_ARGS="/NORESTART /VERYSILENT /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOG=$BUILD_DIRECTORY\git-for-windows.log /LOADINF=$BUILD_DIRECTORY\add_bash.inf /SUPPRESSMSGBOXES /ALLUSERS"
+$GitBash_INSTALL_ARGS="/NORESTART /VERYSILENT /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOG=$BUILD_DIRECTORY\git-for-windows.log /LOADINF=$SETTINGS_FILE /SUPPRESSMSGBOXES /ALLUSERS"
+
+Write-Log "Creating Settings file"
+
+"[Setup]
+Lang=default
+Dir=C:\Program Files\Git
+Group=Git
+NoIcons=0
+SetupType=compact
+Components=icons,icons\desktop,ext,ext\shellhere,ext\guihere,gitlfs,assoc,assoc_sh
+Tasks=
+EditorOption=VIM
+CustomEditorPath=
+DefaultBranchOption=main
+PathOption=Cmd
+SSHOption=OpenSSH
+TortoiseOption=false
+CURLOption=OpenSSL
+CRLFOption=CRLFAlways
+BashTerminalOption=MinTTY
+GitPullBehaviorOption=Merge
+UseCredentialManager=Enabled
+PerformanceTweaksFSCache=Enabled
+EnableSymlinks=Disabled
+EnableBuiltinInteractiveAdd=Disabled
+PrivilegesRequiredOverridesAllowed=commandline
+EnablePseudoConsoleSupport=Disabled
+EnableFSMonitor=Disabled" | Out-File -FilePath $SETTINGS_FILE -Force
 
 Write-Log "Downloading Git Bash"
 Invoke-WebRequest -Uri $GitBash_DOWNLOAD_URL -UseBasicParsing -OutFile "$BUILD_DIRECTORY\$GitBash_INSTALLER_FILE"
