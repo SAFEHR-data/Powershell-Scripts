3097d6b311347996f55d273310fbc80998ab9f5f
David Ramlakhan <dram1964@gmail.com>
2023-04-20 15:36:45 +0000

    adding azcopy installer script

---
+++ add_azcopy.ps1
@@ -0,0 +1,20 @@
+function Write-Log {
+    param (
+        $message
+    )
+    $formattedTime = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+    Write-Host "$formattedTime init_vm: $message"
+}
+
+$BUILD_DIRECTORY="C:\BuildArtifacts"
+$INSTALL_DIRECTORY="C:\Software"
+
+$AZCOPY_INSTALLER_FILE="MicrosoftAzureStorageAzCopy_netcore_x64.msi"
+$AZCOPY_DOWNLOAD_URL="https://aka.ms/downloadazcopy"
+$AZCOPY_INSTALL_ARGS="/qn /jm"
+
+# Write-Log "Downloading AZCOPY installer"
+# Invoke-WebRequest $AZCOPY_DOWNLOAD_URL -OutFile $BUILD_DIRECTORY\$AZCOPY_INSTALLER_FILE
+
+Write-Log "Installing AZCOPY"
+"$BUILD_DIRECTORY\$AZCOPY_INSTALLER_FILE /qn /norestart"
