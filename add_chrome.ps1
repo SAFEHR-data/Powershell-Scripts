23925683008a7e3874a50d9cf9511f9276bd98c4
David Ramlakhan <dram1964@gmail.com>
2024-10-10 15:49:04 +0100

    updates chrome installation to download latest version

--- add_chrome.ps1
+++ add_chrome.ps1
@@ -7,10 +7,9 @@ function Write-Log {
 }
 
 $BUILD_DIRECTORY="C:\BuildArtifacts"
-$INSTALL_DIRECTORY="C:\Software"
 
 $CHROME_INSTALLER_FILE="chrome_installer.exe"
-$CHROME_DOWNLOAD_URL="http://dl.google.com/chrome/install/375.126/$CHROME_INSTALLER_FILE"
+$CHROME_DOWNLOAD_URL="http://dl.google.com/chrome/install/latest/$CHROME_INSTALLER_FILE"
 $CHROME_INSTALL_ARGS="/silent /install"
 
 Write-Log "Downloading CHROME installer..."
