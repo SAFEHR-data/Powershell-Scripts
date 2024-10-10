23b9101fc9e82809bcc790707bf2bc931ee3570b
David Ramlakhan <dram1964@gmail.com>
2024-10-10 11:47:58 +0100

    updates python to 3.13.0

--- add_python3.ps1
+++ add_python3.ps1
@@ -12,8 +12,9 @@ $INSTALL_DIRECTORY="C:\Software"
 Set-Location -Path $BUILD_DIRECTORY
 
 # Python
-$PYTHON_INSTALLER_FILE="python-3.10.7-amd64.exe"
-$PYTHON_DOWNLOAD_URL="https://www.python.org/ftp/python/3.10.7/$PYTHON_INSTALLER_FILE"
+$PYTHON_VERSION="3.13.0"
+$PYTHON_INSTALLER_FILE="python-$PYTHON_VERSION-amd64.exe"
+$PYTHON_DOWNLOAD_URL="https://www.python.org/ftp/python/$PYTHON_VERSION/$PYTHON_INSTALLER_FILE"
 $PYTHON_INSTALL_PATH="$INSTALL_DIRECTORY\Python3"
 $PYTHON_INSTALL_ARGS="/quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=$PYTHON_INSTALL_PATH"
 
@@ -21,6 +22,6 @@ Write-Log "Downloading Python3 installer..."
 Invoke-WebRequest -Uri $PYTHON_DOWNLOAD_URL -UseBasicParsing -OutFile "$BUILD_DIRECTORY\$PYTHON_INSTALLER_FILE"
 
 Write-Log "Installing Python3..."
-Start-Process $PYTHON_INSTALLER_FILE -ArgumentList $PYTHON_INSTALL_ARGS -Wait
+Start-Process $BUIL_DIRECTORY\$PYTHON_INSTALLER_FILE -ArgumentList $PYTHON_INSTALL_ARGS -Wait
 
 Write-Log "add_python3 script completed"
